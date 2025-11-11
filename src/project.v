5039cbbedce2788279bdcab364959ff36522f669
mikkelkofoed0708 <128135162+mikkelkofoed0708@users.noreply.github.com>
2025-11-11 14:33:59 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -6,20 +6,26 @@
 `default_nettype none
 
 module tt_um_mikkelkofoed0708_First_Project (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  [7:0] ui_in,
+    output [7:0] uo_out,
+    input  [7:0] uio_in,
+    output [7:0] uio_out,
+    output [7:0] uio_oe,
+    input        ena,
+    input        clk,
+    input        rst_n
 );
+    reg [23:0] counter;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            counter <= 0;
+        else
+            counter <= counter + 1;
+    end
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uo_out  = counter[23:16];
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
